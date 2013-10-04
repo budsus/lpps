670d167adb94dc73d9ecc359c190baca1e3a9be7
Andreas-LAPTOP <silent_keyboardist@yahoo.com>
2013-10-04 12:18:13 +0700

    ubah nama database di database.php, update file2 modul activity

--- app/Controller/ActivitiesController.php
+++ app/Controller/ActivitiesController.php
@@ -68,6 +68,60 @@ class ActivitiesController extends AppController{
         }
     }
 
+    function download($id){
+        $file=$this->Activity->findById($id);
+        pr($file['Activity']['brosur']);
+        $pathInfo = pathinfo($file['brosur_dir']);
+
+        $this->viewClass = 'Media';
+        $params = array(
+            'id'        => $file['Activity']['brosur'],
+            //'name'      => $pathInfo['filename'],
+            'extension' => 'pdf',
+            'download'  => true,
+            'path'      => APP . 'webroot/files/activity/brosur/'.$file['Activity']['brosur_dir'] . DS
+        );
+        pr($params['path']);
+        $this->set($params);
+
+        //header('Content-type: '.$file['Activity']['filetype']);
+        //header('Content-length: '.$file['Activity']['filesize']);
+        //header('Content-Disposition: attachment; filename="'.$file['Activity']['brosur'].'"');
+
+        //echo $file['File']['data'];
+
+        //exit();
+    }
+
+    /*public function sendFile($id) {
+        $file = $this->Activity->find('first', array(
+            'conditions' => array(
+                'Activity.id' => $id
+            )
+        ));
+        //$file = $this->Attachment->getFile($data['brosur']);
+        $this->response->file($file['brosur_dir'], array(
+            'download' => true//,
+            //'name' => 'the name of the file as it should appear on the client\'s computer',
+        ));
+        //Return reponse object to prevent controller from trying to render a view
+        return $this->response;
+    }*/
+
+    function connect() {
+        $this->Twitter->setupApp('YOUR_CONSUMER_KEY', 'YOUR_CONSUMER_SECRET');
+        $this->Twitter->connectApp('YOUR_CALLBAK_URL');
+    }
+
+    /*public function afterSave($created) {
+        $oauth_token = [either a session or from where the token is saved in db];
+        $oauth_token_secret = [either a session or from where the token is saved in db];
+        $this->Behaviors->load('Twitter.Twitter', array(
+            'oauthToken' => $oauth_token,
+            'oauthTokenSecret' => $oauth_token_secret,
+        ));
+    $this->updateStatus('Some message you want to tweet');
+    }*/
 }
 
 ?>
