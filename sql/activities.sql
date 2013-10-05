5eef9b9ed7a396149a590398443fed8a4df0dcd1
Andreas Unggun Parananda <almightyandreas@gmail.com>
2013-10-05 10:57:07 +0700

    ganti file activities.sql (isi hanya tabel activities)

--- sql/activities.sql
+++ sql/activities.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 02, 2013 at 01:50 AM
+-- Generation Time: Oct 05, 2013 at 05:55 AM
 -- Server version: 5.5.32
 -- PHP Version: 5.4.19
 
@@ -17,10 +17,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `lppsdb`
+-- Database: `lpps`
 --
-CREATE DATABASE IF NOT EXISTS `lppsdb` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-USE `lppsdb`;
 
 -- --------------------------------------------------------
 
@@ -42,7 +40,7 @@ CREATE TABLE IF NOT EXISTS `activities` (
   `brosur` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `brosur_dir` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=11 ;
 
 --
 -- Dumping data for table `activities`
@@ -50,7 +48,8 @@ CREATE TABLE IF NOT EXISTS `activities` (
 
 INSERT INTO `activities` (`id`, `nama_kegiatan`, `penyelenggaraan`, `latar_belakang`, `tujuan`, `sasaran`, `jadwal_kegiatan`, `tempat`, `penanggung_jawab`, `biaya_pendaftaran`, `brosur`, `brosur_dir`) VALUES
 (1, 'Semiloka Koster Gereja', 'Juni 2013', 'Pekerjaan sebagai koster gereja adalah panggilan dari ALlah sendiri. Alkitab menggunakan dst.', 'peserta memiliki pemahaman dan penghayatan tentang perannya dst.', '40 koster gkj dan gki', '17 juni 2013 : Pembukaan, seminar, dst\r\n18 Juni 2013 : talkshow, dst\r\n\r\n', 'aula LPPS', 'Pdt. Wisnu Sapto Nugroho', 'Rp 350.000,00', '', NULL),
-(5, 'Kerja Praktek', 'September-November 2013', 'asdasd', '', '', '', '', '', '', 'JadwalGN1112_21Des.pdf', '5');
+(5, 'Kerja Praktek', 'September-November 2013', 'asdasd', '', '', '', '', '', '', 'JadwalGN1112_21Des.pdf', '5'),
+(10, 'Kegiatan baru', 'ahaha', '', '', '', '', '', '', '', NULL, '');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
