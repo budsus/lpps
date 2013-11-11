063aa6f206c79bfa0b6dfb13db485545e05628d8
Carlo Wardhana <remy.revolrac@gmail.com>
2013-11-11 15:21:59 +0700

    database publikasi

--- sql/publications.sql
+++ sql/publications.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 06, 2013 at 05:57 PM
+-- Generation Time: Nov 11, 2013 at 09:20 AM
 -- Server version: 5.5.32
 -- PHP Version: 5.4.16
 
@@ -25,27 +25,63 @@ USE `lpps`;
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `activities`
+--
+
+CREATE TABLE IF NOT EXISTS `activities` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_kegiatan` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `penyelenggaraan` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `latar_belakang` text COLLATE utf8_unicode_ci,
+  `tujuan` text COLLATE utf8_unicode_ci,
+  `sasaran` text COLLATE utf8_unicode_ci,
+  `jadwal_kegiatan` text COLLATE utf8_unicode_ci,
+  `tempat` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `penanggung_jawab` text COLLATE utf8_unicode_ci,
+  `biaya_pendaftaran` text COLLATE utf8_unicode_ci,
+  `brosur` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `brosur_dir` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `poster` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `poster_dir` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `deskripsi_singkat` varchar(300) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=12 ;
+
+--
+-- Dumping data for table `activities`
+--
+
+INSERT INTO `activities` (`id`, `nama_kegiatan`, `penyelenggaraan`, `latar_belakang`, `tujuan`, `sasaran`, `jadwal_kegiatan`, `tempat`, `penanggung_jawab`, `biaya_pendaftaran`, `brosur`, `brosur_dir`, `poster`, `poster_dir`, `deskripsi_singkat`) VALUES
+(1, 'Semiloka Koster Gereja', 'Juni 2013', 'Pekerjaan sebagai koster gereja adalah panggilan dari ALlah sendiri. Alkitab menggunakan dst.', 'peserta memiliki pemahaman dan penghayatan tentang perannya dst.', '40 koster gkj dan gki', '17 juni 2013 : Pembukaan, seminar, dst\r\n18 Juni 2013 : talkshow, dst\r\n\r\n', 'aula LPPS', 'Pdt. Wisnu Sapto Nugroho', 'Rp 350.000,00', '', NULL, NULL, NULL, 'pendidikan koster yang diselenggarakan tanggal 17-18 Juli dengan biaya pendaftaran Rp 350.000'),
+(5, 'Kerja Praktek', 'September-November 2013', 'asdasd', '', '', '', '', '', '', 'JadwalGN1112_21Des.pdf', '5', NULL, NULL, ''),
+(10, 'Kegiatan baru', 'ahaha', '', '', '', '', '', '', '', NULL, '', NULL, NULL, ''),
+(11, 'Youth Celebration 2013', 'September 2013', '', '', '', '', '', '', '', NULL, '', '1174577_4667825713809_917341112_n.jpg', '11', 'Acara tahunan anak muda GPdI Hagios Family');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `books`
 --
 
 CREATE TABLE IF NOT EXISTS `books` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
+  `cover` varchar(100) DEFAULT NULL,
+  `cover_dir` varchar(100) DEFAULT NULL,
   `judul` varchar(255) DEFAULT NULL,
   `keterangan` varchar(500) DEFAULT NULL,
   `harga` varchar(50) DEFAULT NULL,
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
 
 --
 -- Dumping data for table `books`
 --
 
-INSERT INTO `books` (`id`, `judul`, `keterangan`, `harga`, `created`, `modified`) VALUES
-(3, 'Buku gambar', 'buku gambar monokrom', 'Rp 10000', '2013-09-27 11:10:15', '2013-10-03 11:12:36'),
-(4, 'Kancil', 'Buku tulis', '500', '2013-09-30 08:31:45', '2013-09-30 08:31:45'),
-(5, 'Barang kumal sekali', 'Buku-buku 2', '1500', '2013-09-30 08:33:23', '2013-10-04 07:51:20');
+INSERT INTO `books` (`id`, `cover`, `cover_dir`, `judul`, `keterangan`, `harga`, `created`, `modified`) VALUES
+(15, 'burning-aces-wallpapers_16919_1600x1200.jpg', '15', 'Buku Aces', 'Karangan saya', '4000', '2013-11-04 05:13:48', '2013-11-04 05:13:48'),
+(16, 'dora.png', '16', 'Buku A', 'Karangan saya', '10000', '2013-11-04 06:04:35', '2013-11-04 06:04:35');
 
 -- --------------------------------------------------------
 
@@ -55,20 +91,46 @@ INSERT INTO `books` (`id`, `judul`, `keterangan`, `harga`, `created`, `modified`
 
 CREATE TABLE IF NOT EXISTS `bulletins` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
+  `cover` varchar(100) DEFAULT NULL,
+  `cover_dir` varchar(100) DEFAULT NULL,
   `judul` varchar(255) DEFAULT NULL,
   `keterangan` varchar(1000) DEFAULT NULL,
   `harga` varchar(30) DEFAULT NULL,
+  `periode` varchar(100) NOT NULL,
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
 
 --
 -- Dumping data for table `bulletins`
 --
 
-INSERT INTO `bulletins` (`id`, `judul`, `keterangan`, `harga`, `created`, `modified`) VALUES
-(11, 'Bulletin kancil', 'binantang jalangan', '4000', '2013-10-02 09:33:27', '2013-10-03 08:07:40');
+INSERT INTO `bulletins` (`id`, `cover`, `cover_dir`, `judul`, `keterangan`, `harga`, `periode`, `created`, `modified`) VALUES
+(14, 'hydrangea 2.jpg', '14', 'Hydrangea Bulletin', 'Karangan LPPS', '5000', '2009-2013', '2013-11-10 16:31:20', '2013-11-10 16:31:20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buybulletins`
+--
+
+CREATE TABLE IF NOT EXISTS `buybulletins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `bulletin_id` varchar(255) NOT NULL,
+  `nama` varchar(255) DEFAULT NULL,
+  `notelp` varchar(255) DEFAULT NULL,
+  `keterangantambahan` varchar(255) DEFAULT NULL,
+  `statuspembelian` varchar(100) NOT NULL DEFAULT 'Belum Konfirmasi',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `buybulletins`
+--
+
+INSERT INTO `buybulletins` (`id`, `bulletin_id`, `nama`, `notelp`, `keterangantambahan`, `statuspembelian`) VALUES
+(4, '11', 'Arl', '555', 'tes', 'Belum Konfirmasi');
 
 -- --------------------------------------------------------
 
@@ -82,15 +144,16 @@ CREATE TABLE IF NOT EXISTS `buys` (
   `nama` varchar(255) DEFAULT NULL,
   `notelp` varchar(255) DEFAULT NULL,
   `keterangantambahan` varchar(255) DEFAULT NULL,
+  `statuspembelian` varchar(100) NOT NULL DEFAULT 'Belum Konfirmasi',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
 
 --
 -- Dumping data for table `buys`
 --
 
-INSERT INTO `buys` (`id`, `book_id`, `nama`, `notelp`, `keterangantambahan`) VALUES
-(7, '', 'Carlo', '1245', '1 buah');
+INSERT INTO `buys` (`id`, `book_id`, `nama`, `notelp`, `keterangantambahan`, `statuspembelian`) VALUES
+(12, '16', 'Yose', '777', '1 eksemplar', 'Batal');
 
 -- --------------------------------------------------------
 
@@ -108,14 +171,30 @@ CREATE TABLE IF NOT EXISTS `materials` (
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `materials`
 --
 
 INSERT INTO `materials` (`id`, `judul`, `keterangan`, `materipembinaan_dir`, `filesize`, `filetype`, `created`, `modified`) VALUES
-(6, 'doa.docx', 'lasdjas', '6', NULL, NULL, '2013-10-03 03:02:14', '2013-10-03 03:02:14');
+(6, 'doa.docx', 'lasdjas', '6', NULL, NULL, '2013-10-03 03:02:14', '2013-10-03 03:02:14'),
+(7, 'BORANG edit.docx', 'manon', '7', NULL, NULL, '2013-10-14 14:31:51', '2013-10-14 14:31:51');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `participants`
+--
+
+CREATE TABLE IF NOT EXISTS `participants` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_lengkap` varchar(35) COLLATE utf8_unicode_ci NOT NULL,
+  `alamat` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `telp` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `activity_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
